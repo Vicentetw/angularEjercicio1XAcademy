e25517d3564a9707cfdf5cd7b269b81a9fbe6467
Vicentetw <perrottavicente@gmail.com>
2023-07-16 13:59:13 -0300

    paso còdigo del ts al service list

--- src/app/compontents/pages/home/home.component.ts
+++ src/app/compontents/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { List } from 'src/app/interface/list';
+import { ListService } from 'src/app/services/list.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,22 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'angularEjercicio1XAcademy';
-  list: List[] = [
-    { name: 'Producto 1', cost: '$10' },
-    { name: 'Producto 2', cost: '$20' },
-    { name: 'Producto 3', cost: '$30' }
-  ];
+  lists!: List[];
   showList = true;
 
+constructor(private listService: ListService){}
+ngOnInit(){
+  this.lists = this.listService.getList();
+}
   mostrarList(): void {
     this.showList = !this.showList;
   }
-  getTotalCost(): string {
-    let total = 0;
-    for (const l of this.list) {
-      const costNumber = parseFloat(l.cost.substring(1));
-      total += costNumber;
-    }
-    return `$${total.toFixed(2)}`;
-  }
+  
 }
