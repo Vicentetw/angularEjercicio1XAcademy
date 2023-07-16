9d69cb73003acfae2feb7bd81ad47e33cc585007
Vicentetw <perrottavicente@gmail.com>
2023-07-16 13:45:25 -0300

    agrego interface list y modifico home componet ts

--- src/app/compontents/pages/home/home.component.ts
+++ src/app/compontents/pages/home/home.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'angularEjercicio1XAcademy';
+  list: List[] = [
+    { name: 'Producto 1', cost: '$10' },
+    { name: 'Producto 2', cost: '$20' },
+    { name: 'Producto 3', cost: '$30' }
+  ];
+  showList = true;
+
+  mostrarList(): void {
+    this.showList = !this.showList;
+  }
+  getTotalCost(): string {
+    let total = 0;
+    for (const l of this.list) {
+      const costNumber = parseFloat(l.cost.substring(1));
+      total += costNumber;
+    }
+    return `$${total.toFixed(2)}`;
+  }
 }
